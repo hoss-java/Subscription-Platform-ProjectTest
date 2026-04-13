6b5c4a851e45e63c04aaaecee1ad9e372e174876
Hossein <hossein@internal.nexttop.se>
2026-04-13 22:13:24 +0200

    [B001-C0028] clean up main branch.
    Change-Id: I7e296bbca591446fcb2547b7b46ae03cb6143684

--- subscriptionapi/src/main/java/com/subscriptionapi/exception/InvalidPasswordException.java
+++
@@ -1,7 +0,0 @@
-package com.subscriptionapi.exception;
-
-public class InvalidPasswordException extends RuntimeException {
-    public InvalidPasswordException(String message) {
-        super(message);
-    }
-}
--- subscriptionapi/src/main/java/com/subscriptionapi/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.subscriptionapi.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
