6b2b27e6522de88493b39101c10b986280a58629
Hossein <hossein@internal.nexttop.se>
2026-04-19 16:08:11 +0200

    [B001-C0029] cleanup main branch.
    Change-Id: I8cdc94a485c32aa2ed45ca03bcded946de681816

--- subscriptionapi/src/main/java/com/subscriptionapi/exception/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.subscriptionapi.exception;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-    public EmailAlreadyExistsException(String message) {
-        super(message);
-    }
-}
